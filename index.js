5ff54b89dacf1431b3c4a1834a9b74e92f230726
Oleh Sten <snog190898@yandex.ru>
2017-06-06 16:57:18 +0300

    1

---
+++ index.js
@@ -0,0 +1,20 @@
+var outputElem = document.body;
+ outputElem.innerText = "text";
+
+function getWeather(){
+  var xhr = new XMLHttpRequest;
+
+  xhr.open('GET', 'http://marsweather.ingenology.com/v1/latest/');
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState !== 4) return;
+    if(xhr.status !== 200){
+      alert("error " + xhr.status + ": " + xhr.statusText);
+      return;
+    }
+    console.log(xhr.response)
+//    weather = JSON.parse(xhr.response).report;
+//    outputElem.innerText = "the weather on Mars"
+  }
+  outputElem.innerText = "...";
+  xhr.send();
+}
